ec01fde68feef11e76165e4a24845d8ae636d0e7
ajgorinho10 <kowalskimarek0909@gmail.com>
2025-05-23 22:38:32 +0200

    Teraz działa wszystko

--- Uniterm/MyDrawing.cs
+++ Uniterm/MyDrawing.cs
@@ -59,7 +59,7 @@ namespace Uniterm
 
         public void Redraw()
         {
-            if (oper != ' ')          
+            if (oper != ' ')
             {
                 DrawSwitched(new Point(20, fontsize + 30));
             }
@@ -71,8 +71,7 @@ namespace Uniterm
                 }
                 if (eA != "")
                 {
-                    DrawElim(new Point(30, fontsize * 3 + 30));
-                    //DrawSek(new Point(30, fontsize + 30));
+                    DrawElim(new Point(30, fontsize * 6 + 30));
                 }
             }
         }
@@ -87,26 +86,32 @@ namespace Uniterm
         public void DrawSek(Point pt)
         {
             if (sA == "" || sOp == "") return;
-            int len = GetTextLength(sA + sOp + sB);
 
-            DrawText(pt, sA + sOp + sB);
-            DrawBezier(new Point(pt.X, pt.Y - 1), len);
+            Point p2 = new Point(pt.X + 2, pt.Y);
+            string text = sA + Environment.NewLine.ToString() + sOp + Environment.NewLine.ToString() +
+                sB + Environment.NewLine.ToString();
+
+            double l = GetTextHeight(text) + 2;
+
+            DrawText(p2, text);
+
+            DrawBezier(new Point(pt.X, pt.Y - 1), (int)l);
         }
 
-      public void DrawElim(Point pt)
-        
-      {
+        public void DrawElim(Point pt)
+
+        {
             if (eA == "" || eB == "" || eC == "") return;
 
             Point p2 = new Point(pt.X + 2, pt.Y);
-            string text = eA + " ; " + eB +" ; " + eC;
+            string text = eA + Environment.NewLine.ToString() + ";" + Environment.NewLine.ToString() +
+                eB + Environment.NewLine.ToString() +
+                ";" + Environment.NewLine.ToString() + eC;
 
-            double l = GetTextLength(text);
+            double l = GetTextHeight(text) + 2;
 
-            //DrawText(p2, text);
-            //DrawVert(pt, (int)l);
-            DrawText(pt, text);
-            DrawElHorizontal(pt, (int)l);
+            DrawText(p2, text);
+            DrawVert(pt, (int)l);
         }
 
         public void DrawSwitched(Point pt)
@@ -114,27 +119,35 @@ namespace Uniterm
             if (sA == "" || sOp == "" || eA == "" || eB == "" || eC == "") return;
 
 
-            string textElim = eA + " ; " + eB + " ; " + eC;
+            string textElim = eA + Environment.NewLine.ToString() + ";" + Environment.NewLine.ToString() +
+                eB + Environment.NewLine.ToString() +
+                ";" + Environment.NewLine.ToString() + eC;
 
-            int length = GetTextLength(textElim);
 
-            string tmpOp = " " + sOp + " ";
-            Console.WriteLine("sOp:"+ tmpOp);
+            int length = GetTextHeight(textElim)+ 10;
+
 
             if (oper == 'A')
             {
-                DrawText(new Point(pt.X + length + (fontsize / 3), pt.Y + 3), tmpOp + sB);
-                DrawElim(new Point(pt.X + (fontsize / 3), pt.Y + 5));
-                length += GetTextLength(tmpOp + sB) + (int)(fontsize / 3);
+                string text = sOp + Environment.NewLine.ToString() + sB;
+ 
+                DrawText(new Point(pt.X + (fontsize / 3) - 2, pt.Y + length + (fontsize / 3)), sOp);
+                DrawText(new Point(pt.X + GetTextLength(sOp) - 2, pt.Y + length + (fontsize / 3) + GetTextHeight(sOp)), sB);
+
+                DrawElim(new Point(pt.X + GetTextLength(text), pt.Y + (fontsize / 3) + 4));
+
+                length += GetTextHeight(text) + (int)(fontsize / 3);
             }
             if (oper == 'B')
             {
-                int sekTextLenght = GetTextLength(sA+tmpOp)+10;
+                string text = sA + Environment.NewLine.ToString() + sOp;
 
-                DrawText(new Point(pt.X+5,pt.Y), sA + tmpOp+ " ");
-                DrawElim(new Point(pt.X + sekTextLenght + (fontsize / 3), pt.Y + 5));
+                DrawText(new Point(pt.X + GetTextLength(sOp) - 2 - 2, pt.Y + (fontsize / 3)), sA);
+                DrawText(new Point(pt.X + (fontsize / 3) - 2, pt.Y + (fontsize / 3) + GetTextHeight(sA) -3), sOp);
 
-                length += GetTextLength(sOp + sB) + (int)(fontsize / 3)+10;
+                DrawElim(new Point(pt.X + GetTextLength(text), pt.Y + (fontsize / 3) + 5 + GetTextHeight(text)));
+
+                length += GetTextHeight(text) + (int)(fontsize / 3);
             }
 
             DrawBezier(pt, length + 5);
@@ -144,21 +157,31 @@ namespace Uniterm
 
         #region Private Methods
 
+        private void DrawVert(Point pt, int length)
+        {
+            dc.DrawLine(pen, pt, new Point { X = pt.X, Y = pt.Y + length });
+            double b = (Math.Sqrt(length) / 2) + 2;
+
+            dc.DrawLine(pen, new Point(pt.X - (b / 2), pt.Y), new Point(pt.X + (b / 2), pt.Y));
+            dc.DrawLine(pen, new Point(pt.X - (b / 2), pt.Y + length), new Point(pt.X + (b / 2), pt.Y + length));
+
+        }
+
         private void DrawBezier(Point p0, int length)
         {
             Point start = p0;
             Point p1 = new Point(), p2 = new Point(), p3 = new Point();
 
-            p3.Y = p0.Y;
-            p3.X = p0.X + length;
+            p3.Y = p0.Y + length;
+            p3.X = p0.X;
 
             int b = (int)Math.Sqrt(length) + 2;
 
-            p1.X = p0.X + (int)(length * 0.25);
-            p1.Y = p0.Y - b;
+            p1.X = p0.X - b;
+            p1.Y = p0.Y + (int)(length * 0.25);
 
-            p2.X = p0.X + (int)(length * 0.75);
-            p2.Y = p0.Y - b;
+            p2.X = p0.X - b;
+            p2.Y = p0.Y + (int)(length * 0.75);
 
             foreach (Point pt in GetBezierPoints(p0, p1, p2, p3))
             {
@@ -167,25 +190,14 @@ namespace Uniterm
             }
         }
 
-        private void DrawElHorizontal(Point p0,int length)
-        {
-            Point start = p0;
-            Point end  = new Point();
-            end.X = p0.X + length;
-            end.Y = p0.Y;
-
-            double b = (Math.Sqrt(length) / 2) + 4;
-
-            dc.DrawLine(pen, new Point(start.X, start.Y - (b / 2)), new Point(start.X, start.Y + (b / 2)));
-            dc.DrawLine(pen, new Point(end.X, end.Y - (b / 2)), new Point(end.X, end.Y + (b / 2)));
-
-            dc.DrawLine(pen, start, end);
-
-        }
-
         private void DrawText(Point point, string text)
         {
             dc.DrawText(GetFormattedText(text), point);
+        }
+
+        private int GetTextHeight(string text)
+        {
+            return (int)GetFormattedText(text).Height;
         }
 
         private int GetTextLength(string text)
@@ -210,7 +222,7 @@ namespace Uniterm
                         + 3 * t * tas * B.Y
                         + 3 * tbs * (1 - t) * C.Y
                         + tbc * D.Y,
-                   X = +tac * A.X
+                    X = +tac * A.X
                         + 3 * t * tas * B.X
                         + 3 * tbs * (1 - t) * C.X
                         + tbc * D.X
